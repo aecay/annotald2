2856be77c016c65cc8c6a674f4ed10689f2e60c5
Aaron Ecay <aaronecay@gmail.com>
2016-12-01 18:47:03 +0000

    Actions.elm: start writing docs, make Silent failures have a debugging message

--- src/Actions.elm
+++ src/Actions.elm
@@ -5,37 +5,75 @@ module Actions exposing ( clearSelection
                         , liftMaybe, Failure(..), Result -- TODO: don't want to export
                         )
 
+{-| This module contains the types and functions for creating *actions*, or
+functions that respond to user input.
+
+# The Result type
+
+@docs Failure, Result, Action
+
+## Operations on Results
+
+@docs succeed, fail, fmap, bind, liftMaybe, flattenMaybe
+
+# TODO
+
+ -}
+
+-- Standard library
+
 import Maybe exposing (withDefault)
-import Maybe.Extra
---import List.Extra exposing (singleton)
---import List exposing (head, tail)
-
-import Tree exposing (Path, Tree)
-
-import Utils exposing ((?>), (?>?), zip)
-
 import Dict exposing (Dict)
-
-import TreeExts as TX
-import MultiwayTree as MT
-
-import Model exposing (Model, refresh)
-import Selection
 import Result as R
 
+-- Third party
 
-type Failure = Silent | Msg String
+import Maybe.Extra
+import MultiwayTree as MT
 
+-- Annotald packages
+
+import Tree exposing (Path, Tree)
+import Utils exposing ((?>), (?>?), zip)
+import TreeExts as TX
+import Model exposing (Model, refresh)
+import Selection
+
+-- Result types and functions
+
+{-| This type represents failures during the execution of a user command.
+
+The `Silent` failure type comes with a message, which is counterintuitive but
+useful for debugging.  (Unlike for the `Msg` type, `Silent` failures are not
+displayed to the user.)
+
+-}
+type Failure = Silent String | Msg String
+
+{-| This type represents the result of applying a user command.
+
+The command can succeed with a new model (which replaces the old one), or fail
+with a `Failure`.
+-}
 type alias Result = R.Result Failure Model
 
-type alias Action = Model -> Result
-
+{-| Other sections of Annotald are written as functions operating on trees,
+not in a user interaction context.  These signal success or failure with
+`Maybe`.  This function "lifts" those values into the `Result` context, making
+them suitable for use in user interaction functions.
+-}
 liftMaybe : Failure -> Maybe a -> R.Result Failure a
 liftMaybe f m =
     case m of
         Just m -> R.Ok m
         Nothing -> R.Err f
 
+{-| A similar problem occurs when we already have a `Result` and wish to apply
+one of the non-user functions to it, but we get the `Maybe` inside the
+`Result` (rather than instead of it, as in the case where we want to
+`liftMaybe`).  This function takes a `Maybe` embedded inside a `Result` and
+turns it into just a result.
+-}
 flattenMaybe : Failure -> R.Result Failure (Maybe a) -> R.Result Failure a
 flattenMaybe failure resultMaybe =
     case resultMaybe of
@@ -50,24 +88,38 @@ succeed = R.Ok
 fail : Failure -> Result
 fail = R.Err
 
+{-| Fmap for `Result`.
+
+See also the [Functor typeclass](https://wiki.haskell.org/Typeclassopedia#Functor).
+-}
 fmap : (a -> b) -> R.Result Failure a -> R.Result Failure b
 fmap f result =
     case result of
         R.Ok m -> R.Ok (f m)
         R.Err e -> R.Err e
 
+{-| Monadic bind for `Result`.
+
+See also the [Monad
+typeclass](https://wiki.haskell.org/Typeclassopedia#Monad).  (`return` is
+given by `succeed`.)
+-}
 bind : (a -> R.Result Failure b) -> R.Result Failure a -> R.Result Failure b
 bind f result =
     case result of
         R.Ok a -> f a
         R.Err e -> R.Err e
 
+-- The rest of the file
+
+type alias Action = Model -> Result
+
 doOneSelected : (Tree -> Tree) -> Model -> Result
 doOneSelected f model =
     let
         path = model.selected |>
                Selection.first
-    in liftMaybe Silent path |>
+    in liftMaybe (Silent "doOneSelected") path |>
     bind (\x -> doAt x f model)
 
 doAt : Path -> (Tree -> Tree) -> Model -> Result
@@ -77,10 +129,10 @@ doAt path f model =
     in
         root |>
         Tree.get path |>
-        liftMaybe Silent |>
+        liftMaybe (Silent "doAt 1") |>
         fmap f |>
         fmap (\x -> Tree.set path x root) |>
-        flattenMaybe Silent |>
+        flattenMaybe (Silent "doAt 2") |>
         fmap (refresh model)
 
 clearSelection : Action
@@ -113,7 +165,7 @@ coIndex model =
         case (first, second) of
             (Just f, Just s) ->
                 coIndex2 f s model
-            otherwise -> fail Silent
+            otherwise -> fail (Silent "coIndex")
 
 coIndex2 : Path -> Path -> Model -> Result
 coIndex2 f s model =
