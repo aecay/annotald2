74700753480f29220d642c1450491f208f4f70e8
Aaron Ecay <aaronecay@gmail.com>
2017-07-18 00:31:30 +0100

    python server

---
+++ python/main.py
@@ -0,0 +1,49 @@
+# filename: main.py
+import hug
+import click
+import lovett.corpus
+
+import glob
+import os
+
+
+CORPUS_PATH = "/home/aecay/projects/chlg/parsing"
+
+
+html = hug.get(output=hug.output_format.html)
+
+
+# @hug.get('/')
+# def index():
+#     with open('static/index.html') as fin:
+#         return fin.read()
+
+
+@hug.static("/static")
+def static():
+    return (os.path.join(os.path.dirname(__file__), "static"),)
+
+
+@hug.get('/files')
+def files():
+    return list(map(lambda x: os.path.relpath(x, CORPUS_PATH),
+                    glob.glob(os.path.join(CORPUS_PATH, "*.psd"))))
+
+
+@hug.get("/file")
+def get_file(name: hug.types.text):
+    path = os.path.join(CORPUS_PATH, name)
+    with open(path) as fin:
+        corpus = lovett.corpus.from_file(fin)
+    return [tree.to_json() for tree in corpus]
+
+
+@click.command()
+@click.option("--path", type=click.Path())
+def main(path):
+    global CORPUS_PATH
+    CORPUS_PATH = path
+
+
+if __name__ == "__main__":
+    main()
