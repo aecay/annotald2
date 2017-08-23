cefbe3d9a2a800375ee1a11f947132d79c3250a1
Aaron Ecay <aaronecay@gmail.com>
2017-08-23 17:24:31 +0100

    fix writing of metadata to json

--- src/TreeEdit/Tree/Json.elm
+++ src/TreeEdit/Tree/Json.elm
@@ -1,5 +1,6 @@
 module TreeEdit.Tree.Json exposing (toJson)
 
+import Dict
 import Json.Encode as E exposing (Value)
 
 import TreeEdit.Tree.Type exposing (Tree, TraceType(..), Node(..))
@@ -12,36 +13,45 @@ list_ fn l = E.list <| List.map fn l
 toJson : List Tree -> Value
 toJson trees = list_ treeToJson trees
 
-indexToJson : Maybe Index -> List (String, Value)
+indexToJson : Maybe Index -> List (String, String)
 indexToJson idx =  case idx of
                        Nothing -> []
-                       Just i -> [("metadata", E.object
-                                       [ ("INDEX", .get Index.number i |> toString |> E.string)
-                                       , ("IDX-TYPE", E.string <| case .get Index.variety i of
-                                                                      Index.Normal -> "normal"
-                                                                      Index.Gap -> "gap")]
-                                  )]
+                       Just i -> [ ("INDEX", .get Index.number i |> toString)
+                                 , ("IDX-TYPE", case .get Index.variety i of
+                                                    Index.Normal -> "normal"
+                                                    Index.Gap -> "gap")
+                                 ]
+
 
 treeToJson : Tree -> Value
 treeToJson tree =
-    case tree.contents of
-        Nonterminal children idx ->
-            let
-                kids = [("children", E.list <| List.map treeToJson children)]
-            in
-                E.object <| [ ("label", E.string tree.label) ] ++ kids ++ indexToJson idx
-        Terminal text index -> E.object <| [ ("label", tree.label |> E.string)
-                                           , ("text", text |> E.string)
-                                           ] ++ (indexToJson index)
-        Comment string -> E.object [ ("label", "CODE" |> E.string)
-                                   , ("text", string |> E.string)
-                                   ]
-        EmptyCat _ index -> E.object <| [ ("label", tree.label |> E.string)
-                                        , ("text", terminalString tree |> E.string)
-                                        ] ++ (indexToJson index)
-        Trace traceType index -> E.object <| [ ("label", tree.label |> E.string)
-                                             , ("text", E.string <| case traceType of
-                                                                        Wh -> "*T*"
-                                                                        Extraposition -> "*ICH*"
-                                                                        Clitic -> "*CL*")
-                                             ] ++ (indexToJson <| Just <| Index.normal index)
+    let
+        metadata idx = E.object <| List.map (\(k, v) -> (k, E.string v)) <| (Dict.toList tree.metadata) ++ (indexToJson idx)
+    in
+        case tree.contents of
+            Nonterminal children idx ->
+                let
+                    kids = []
+                in
+                    E.object <| [ ("label", E.string tree.label)
+                                , ("children", E.list <| List.map treeToJson children)
+                                , ("metadata", metadata idx)
+                                ]
+            Terminal text index -> E.object <| [ ("label", tree.label |> E.string)
+                                               , ("text", text |> E.string)
+                                               , ("metadata", metadata index)
+                                               ]
+            Comment string -> E.object [ ("label", "CODE" |> E.string)
+                                       , ("text", string |> E.string)
+                                       ]
+            EmptyCat _ index -> E.object <| [ ("label", tree.label |> E.string)
+                                            , ("text", terminalString tree |> E.string)
+                                            , ("metadata", metadata index)
+                                            ]
+            Trace traceType index -> E.object <| [ ("label", tree.label |> E.string)
+                                                 , ("text", E.string <| case traceType of
+                                                                            Wh -> "*T*"
+                                                                            Extraposition -> "*ICH*"
+                                                                            Clitic -> "*CL*")
+                                                 , ("metadata", metadata <| Just <| Index.normal index)
+                                                 ]
