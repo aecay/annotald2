db3bdfd8564f7d3efc5420269c19ed6a40c4f185
Aaron Ecay <aaronecay@gmail.com>
2017-12-04 13:13:52 +0000

    clean up toolbar code

--- src/TreeEdit/View/ToolBar.elm
+++ src/TreeEdit/View/ToolBar.elm
@@ -1,35 +1,43 @@
 module TreeEdit.View.ToolBar exposing (view)
 
-import Html exposing (Html, div, text, br, button)
+import Html exposing (Html, div, text, br)
 import Html.Attributes as A
-import Html.Events exposing (onClick)
+import Html.Events as E
 
 import TreeEdit.Msg as Msg exposing (Msg)
 import TreeEdit.View.Theme exposing (theme)
 
+box : String -> List (Html Msg) -> Html Msg
+box title contents =
+    div []
+        [ div [ A.style [ ("background-color", theme.darkGrey)
+                        , ("color", "white")
+                        , ("width", "100%")
+                        , ("height", "16px")
+                        , ("font-weight", "bold")
+                        , ("text-align", "center")
+                        ]
+              ] [ text title ]
+        , div [ A.style [ ("background-color", theme.offWhite2)
+                        , ("padding-bottom", "4px")
+                        , ("border", "1px solid " ++ theme.darkTan)
+                        , ("margin-bottom", "6px")
+                        , ("text-align", "center")
+                        ]
+              ] contents
+        ]
+
+button : String -> Msg -> Html Msg
+button txt click = Html.button [ E.onClick click,  A.style [ ("width", "80%") ] ] [ text txt ]
+
 view : String -> Html Msg
-view filename  = div []
-                 [ div [ A.style [ ("background-color", theme.darkGrey)
-                                 , ("color", "white")
-                                 , ("width", "100%")
-                                 , ("height", "16px")
-                                 , ("font-weight", "bold")
-                                 , ("text-align", "center")
-                                 ]
-                       ] [ text "Annotald 2" ]
-                 , div [ A.style [ ("background-color", theme.offWhite2)
-                                 , ("padding-bottom", "4px")
-                                 , ("border", "1px solid " ++ theme.darkTan)
-                                 , ("margin-bottom", "6px")
-                                 , ("text-align", "center")
-                               ]
-                       ]
-                     [ text ("Editing " ++ filename)
-                     , button [ onClick Msg.Save
-                              , A.style [ ("width", "80%") ]
-                              ]
-                           [text "Save"]
-                     -- Undo, redo, exit
-                     ]
-                 -- Search, validate
-                 ]
+view filename =
+    div []
+        [ box "Annotald 2"
+              [ text ("Editing " ++ filename)
+              , button "Save" Msg.Save
+              -- Undo, redo, exit
+              ]
+        , box "Tools"
+            [ button "Validate" Msg.Validate ]
+        ]
