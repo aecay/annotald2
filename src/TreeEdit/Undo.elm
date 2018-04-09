830fed0c392a60d6f05fb750aa9e99862d6668ae
Aaron Ecay <aaronecay@gmail.com>
2018-04-09 02:08:26 +0100

    undo: simplify code

--- src/TreeEdit/Undo.elm
+++ src/TreeEdit/Undo.elm
@@ -1,45 +1,37 @@
 module TreeEdit.Undo exposing (undo, redo)
 
-import Return exposing (Return, singleton)
+import Cmd.Extra
+import Return exposing (Return, singleton, return)
 
 import TreeEdit.Model as Model
 import TreeEdit.Model.Type exposing (Model)
-import TreeEdit.Msg exposing (Msg)
+import TreeEdit.Msg exposing (Msg(LogMessage))
 
 undo : Model -> Return Msg Model
-undo m =
-    let
-        undo = m.undo
-        h = List.head undo
-        r = List.tail undo |> Maybe.withDefault []
-    in
-        case h of
-            Nothing -> singleton { m | lastMessage = "No undo information" }
-            Just tree ->
-                let
-                    redoTree = .get Model.root m
-                    newModel = .set Model.root tree m
-                in
-                    singleton { newModel |
-                                undo = r
-                              , redo = redoTree :: newModel.redo
-                              }
+undo ({undo, redo} as m) =
+    case undo of
+        [] -> return m <| Cmd.Extra.perform <| LogMessage "No undo information"
+        tree :: rest ->
+            let
+                redoTree = .get Model.root m
+                newModel = .set Model.root tree m
+            in
+                singleton { newModel |
+                            undo = rest
+                          , redo = redoTree :: redo
+                          }
+
 
 redo : Model -> Return Msg Model
-redo m =
-    let
-        redo = m.redo
-        h = List.head redo
-        r = List.tail redo |> Maybe.withDefault []
-    in
-        case h of
-            Nothing -> singleton { m | lastMessage = "No redo information" }
-            Just tree ->
-                let
-                    undoTree = .get Model.root m
-                    newModel = .set Model.root tree m
-                in
-                    singleton { newModel |
-                                undo = undoTree :: newModel.undo
-                              , redo = r
-                              }
+redo ({undo, redo} as m) =
+    case redo of
+        [] -> return m <| Cmd.Extra.perform <| LogMessage "No undo information"
+        tree :: rest ->
+            let
+                undoTree = .get Model.root m
+                newModel = .set Model.root tree m
+            in
+                singleton { newModel |
+                            redo = rest
+                          , undo = undoTree :: undo
+                          }
