6b009bef598e357e7d4c543d7724a73337d07af5
Aaron Ecay <aaronecay@gmail.com>
2018-05-22 18:38:48 +0100

    docker stuff: update

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
-# TODO: elm build
+set -e
 
-rsync --exclude .git ~/projects/lovett .
-cp ../python/requirements.txt .
-cp ../python/main.py .
-cp -r ../python/static .
+rm -rf deps
 
-sudo docker build -t annotald .
+mkdir deps
+
+( cd ~/projects/lovett;
+  python setup.py sdist;
+)
+
+LOVETT_VERSION=$( cd ~/projects/lovett;
+  echo `python setup.py -V`
+)
+
+cp ~/projects/lovett/dist/lovett-${LOVETT_VERSION}.tar.gz deps
+
+cp ~/projects/lovett/requirements.txt deps/lovett-requirements.txt
+cp ../python/requirements.txt deps
+cp ../python/main.py deps
+cp -r ../python/static deps
+
+rm deps/static/elm.js
+
+(cd ..; elm-make src/Main.elm --yes --warn --output=docker/deps/static/elm.js)
+
+sudo docker build -t aecay/annotald:testing .
--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+HERE=`readlink -f .`
+
 docker run \
-       --mount "type=bind,src=.,dst=/data" \
-       annotald $@
+       --mount "type=bind,src=${HERE},dst=/data" \
+       -p 8000:8000 \
+       annotald:latest python /annotald/main.py --docker $@
